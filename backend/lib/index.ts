3aa05d7f98509b3a8f4ce948d1d4a43319103767
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-03-06 22:53:36 -0600

    BE: Server up and running.

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,1 +1,6 @@
-console.log("hello world");
+import app from "./app"
+
+const port = process.env.APP_PORT || 3001
+app.listen(port,()=>{
+    console.log("The Playlist is running on port: " + port);
+})
