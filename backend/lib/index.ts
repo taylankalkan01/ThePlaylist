b449a85363bdd9909e9f23ad4b7a545d0c65f53b
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-03-06 22:59:32 -0600

    BE: Prettier added.

--- backend/lib/index.ts
+++ backend/lib/index.ts
@@ -1,6 +1,6 @@
-import app from "./app"
+import app from "./app";
 
-const port = process.env.APP_PORT || 3001
-app.listen(port,()=>{
-    console.log("The Playlist is running on port: " + port);
-})
+const port = process.env.APP_PORT || 3001;
+app.listen(port, () => {
+  console.log("The Playlist is running on port: " + port);
+});
