36b3e8885e6c6118cf4ef10d993c19a55eb880a9
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-03-08 22:12:32 -0600

    BE: Admin Register endpoint added.

--- backend/lib/controllers/authController.ts
+++ backend/lib/controllers/authController.ts
@@ -45,7 +45,50 @@ const registerUser = async (req: Request, res: Response) => {
       });
   }
 };
+const registerAdmin = async (req: Request, res: Response) => {
+  const { firstName, lastName,password, dob, email} = req.body;
+  try {
+    //validation TODO
+
+    //check if email is exists
+    const admin = await User.findOne({ email });
+    if (admin) {
+      return res.status(400).json({
+        error: true,
+        message: "You cannot create admin, Email already exist"
+      });
+    }
+    //hash password
+    const saltPass = await bcrypt.genSalt(10);
+    const hashPass = await bcrypt.hash(password, saltPass);
+
+    //register admin
+    const newAdmin = new User({
+      firstName,
+      lastName,
+      dob,
+      email,
+      password: hashPass,
+      isAdmin:true
+    });
+
+    const data = await newAdmin.save();
+
+    res.status(201).json({
+      error: false,
+      message: "Admin Account Created Succesfully!",
+      data: data
+    });
+  } catch (err) {
+    res
+      .status(500)
+      .json({
+        error: true,
+        message: `${process.env.NODE_ENV === "production" ? null : err}`
+      });
+  }
+};
 
 export default {
-  registerUser
+  registerUser,registerAdmin
 };
--- backend/lib/routes/authRoutes.ts
+++ backend/lib/routes/authRoutes.ts
@@ -4,5 +4,6 @@ import authController from "../controllers/authController";
 const router = Router();
 
 router.post("/user/register", authController.registerUser);
+router.post("/admin/register", authController.registerAdmin);
 
 export default router;
