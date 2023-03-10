18f20b2422d7bda8072a321c7ffc22fba1a96fac
taylankalkan01 <e.taylankalkan@hotmail.com>
2023-03-10 15:39:36 -0600

    ADMINPANEL: Home page added.

--- admin/src/pages/home/Home.jsx
+++ admin/src/pages/home/Home.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import "./home.scss";
 import Sidebar from "../../components/sidebar/Sidebar";
+import Navbar from "../../components/navbar/Navbar";
+import TotalUser from "../../components/Dashboard/total-user/TotalUser";
+import Chart from "../../components/Dashboard/chart/Chart";
+import Widget from "../../components/Dashboard/widgets/Widget";
 
-export default function Home() {
-  return <Sidebar />;
-}
+const Home = () => {
+  return (
+    <div className="home">
+      <Sidebar />
+      <div className="homeContainer">
+        <Navbar />
+        <div className="widgets">
+          <Widget type="artists" />
+          <Widget type="albums" />
+          <Widget type="earnings" />
+          <Widget type="new-songs" />
+        </div>
+        <div className="charts">
+          <TotalUser />
+          <Chart />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
